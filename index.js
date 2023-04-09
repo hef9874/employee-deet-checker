8ddeccd35e867c0c391d9d846ca59691170d36ed
nah <hef9874@gmail.com>
2023-04-09 15:22:44 -0400

    changes to index.js and adding lib and src

--- index.js
+++ index.js
@@ -1,107 +1,40 @@
-const { query } = require("express");
-const express = require("express");
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
-require("console.table");
-require('dotenv').config();
+const fs = require("fs");
+const generateHTML = require('./src/generateHTML');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// importing subclasses
+const Manager = require('./lib/Manager');
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee")
 
-//express 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const path = require("path");
+const newPath = path.resolve(__dirname, "dist");
+const teamHTML = path.join(newPath, 'team.html');
 
-//Database connection
-const db = mysql.createConnection(
-    {
-      host: "127.0.0.1",
-      // MySQL username,
-      user: "root",
-      password: process.env.DB_PASSWORD,
-      database: "employee_db",
-    },
-    console.log(`Database Connected.`)
-);
+// array to add team members to
+const teamInfo = [];
 
-//If database is connected, start prompts
-
-
-
-//show main menu prompts
-const showPrompts = () => {
-        return inquirer.prompt ([
+async function init() {
+    const managerInput = [
         {
-            type: 'list',
-            message: "Choose an option",
-            name: "Choices",
-            choices: [
-                "View Departments",
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                'Update an employee manager',
-                'View all employees by manager',
-                'View all employees by department',
-                "View budget",
-                "Delete department",
-                "Delete role",
-                "Delete employee",
-                'Quit',
-            ]
+            type: 'input',
+            message: 'manager name- ',
+            name: 'name',
+        },
+        {
+            type:'input',
+            message: 'manager id- ',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'manager email- ',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'manager office # - '
         }
     ]
-        )};
-
-//start specific function depending on the user answer
-switch (userAnswer.startPrompt) {
-    case "View Departments":
-        viewDepartments();
-        break;
-    case "View all roles":
-        viewRoles();
-        break;
-    case "View all employees":
-        viewEmployess();
-    break;
-    case "Add a department":
-        addDepartment();
-    break;
-    case "Add a role":
-        addRole();
-    break;
-    case "Add an employee":
-        addEmployee();
-    break;
-    case "Update an employee role":
-        updateRole();
-    break;
-    case "Update an employee manager":
-        updateManager();
-    break;
-    case "View all employees by manager":
-        viewEmployeesbyManager();
-        break;
-    
-
 }
-
-//function to show dept names from db
-function viewDepartments(res) {
-    const departments, (err, res) => {
-        if (err) throw err;
-        console.log("Departments:");
-        console.table(res);
-        showPrompts()
-    }
-}
-
-//function to view roles 
-
-
-app.listen(PORT, () => {
-    console.log("Employee Deets Checker is connected.");
-  });
